73e801b5470c3aec8826deb0635868b22590b01e
Pranav1483 <ch20b082@smail.iitm.ac.in>
2024-03-12 16:55:19 +0530

    Change login method

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,7 +9,7 @@ import axios from "axios";
 export default function LoginPage() {
 
   const router = useRouter();
-  const [creds, setCreds] = useState({username: "", password: ""});
+  const [creds, setCreds] = useState({email: "", password: ""});
   const accessToken = Cookies.get("huesAccessToken");
   const refreshToken = Cookies.get("huesRefreshToken");
 
@@ -20,7 +20,7 @@ export default function LoginPage() {
   }, [])
   
   const signIn = () => {
-    axios.post(`${BASE_URL}/v2/signin`, creds)
+    axios.post(`${BASE_URL}/v1/signin`, creds)
     .then((response) => {
       Cookies.set("huesAccessToken", response.data.access);
       Cookies.set("huesRefreshToken", response.data.refresh);
@@ -35,7 +35,7 @@ export default function LoginPage() {
     <div className="h-screen">
       <div className="h-screen left-0">
         <div className="container mx-auto flex flex-col items-center justify-center relative w-full h-screen bg-white gap-2">
-          <input type="text" placeholder="Email" value={creds.username} onChange={e => setCreds({...creds, username: e.target.value})}/>
+          <input type="text" placeholder="Email" value={creds.email} onChange={e => setCreds({...creds, email: e.target.value})}/>
           <input type="password" placeholder="Password" value={creds.password} onChange={e => setCreds({...creds, password: e.target.value})}/>
           <button className="p-3 bg-blue-500 text-white font-bold rounded-full" onClick={signIn}>Sign In</button>
           <a href="/forgot" className="underline text-sm">Forgot Password</a>
