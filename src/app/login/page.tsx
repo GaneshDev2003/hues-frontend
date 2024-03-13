4d20d67c7c4618775611a73344e23d69dd623817
Pranav1483 <ch20b082@smail.iitm.ac.in>
2024-03-13 16:43:11 +0530

    Changed to Next-Auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,44 +1,55 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import AppBar from "@/components/appbar";
+import { getSession, signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { BASE_URL } from "@/utils/api";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 import axios from "axios";
 
-
 export default function LoginPage() {
-
   const router = useRouter();
-  const [creds, setCreds] = useState({email: "", password: ""});
-  const accessToken = Cookies.get("huesAccessToken");
-  const refreshToken = Cookies.get("huesRefreshToken");
+  const { data: session, status } = useSession();
 
-  useEffect(() => {
-    if (accessToken && refreshToken) {
-      window.location.href = "/home";
-    }
-  }, [])
-  
-  const signIn = () => {
-    axios.post(`${BASE_URL}/v1/signin`, creds)
-    .then((response) => {
-      Cookies.set("huesAccessToken", response.data.access);
-      Cookies.set("huesRefreshToken", response.data.refresh);
+  const getSessionData = async () => {
+    if (session && session.user) {
+      axios.post(`${BASE_URL}/v1/user`, {email: session.user?.email})
+      .then((response) => {
+        Cookies.set("huesAccessToken", response.data.access);
+        Cookies.set("huesRefreshToken", response.data.refresh);
+      })
+      .catch((e) => {
+        
+      })
       router.push("/home");
-    })
-    .catch((e) => {
-
-    });
-  }
-
+    }
+  };
+  useEffect(() => {
+    getSessionData();
+  });
   return (
     <div className="h-screen">
       <div className="h-screen left-0">
-        <div className="container mx-auto flex flex-col items-center justify-center relative w-full h-screen bg-white gap-2">
-          <input type="text" placeholder="Email" value={creds.email} onChange={e => setCreds({...creds, email: e.target.value})}/>
-          <input type="password" placeholder="Password" value={creds.password} onChange={e => setCreds({...creds, password: e.target.value})}/>
-          <button className="p-3 bg-blue-500 text-white font-bold rounded-full" onClick={signIn}>Sign In</button>
-          <a href="/forgot" className="underline text-sm">Forgot Password</a>
+        <div className="container mx-auto flex flex-col items-center justify-center relative w-full h-screen bg-white">
+          <div className=" mx-auto">
+            <img
+              className="mt-10 object-cover"
+              alt="Screenshot"
+              src="images/logo.png"
+            />
+            <div className="flex items-center">
+              <div className="flex-grow border-b border-gray-400"></div>
+              <div className="px-4 text-gray-500">Login with</div>
+              <div className="flex-grow border-b border-gray-400"></div>
+            </div>
+          </div>
+          <button onClick={() => signIn()}>
+            <img
+              className="my-3 mx-auto"
+              alt="Soc med"
+              src="images/SocMed.png"
+            />
+          </button>
         </div>
       </div>
     </div>
