5c8a02fb82a6e8325664efaff80d642a149a410d
GaneshDev2003 <me20b070@smail.iitm.ac.in>
2024-04-06 03:54:04 +0530

    changed notifications

--- service-worker/index.ts
+++ service-worker/index.ts
@@ -3,7 +3,6 @@
 self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   if (Notification.permission == 'granted') {
-    reminderNotification();
   }
 });
 
@@ -19,8 +18,40 @@ declare var registration: any;
 console.log('Custom Service Worker');
 const reminderNotification = () => {
   registration.showNotification('Reminder', {
-    body: 'Hey there! How are you feeling today? Time to be expressive about it!',
+    body: 'Hey there! How are you feeling today? Time to be expressive about it !',
     icon: 'icons/icons.png',
   });
   setTimeout(reminderNotification, 86400000);
 };
+
+// declare var registration: any;
+// console.log('Custom Service Worker');
+// const reminderNotification = () => {
+//   if (Notification.permission == 'granted') {
+//     console.log(navigator);
+//     var options = {
+//       body: 'Welcome!',
+//       icon: 'Hey there! How are you feeling today? Time to be creative about it!',
+//       vibrate: [100, 50, 100],
+//       data: {
+//         dateOfArrival: Date.now(),
+//         primaryKey: 1,
+//       },
+//     };
+//     registration.showNotification('Reminder', options);
+//   }
+// };
+
+navigator.permissions.query({ name: 'notifications' }).then((status) => {
+  if (status.state === 'prompt') {
+    status.onchange = () => {
+      if (status.state === 'granted') {
+        reminderNotification();
+      }
+    };
+    // @ts-ignore
+    document
+      .querySelector('#notification')
+      .addEventListener('click', () => Notification.requestPermission());
+  }
+});
